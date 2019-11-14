f8f72d2421669ac7721f6d9be66b7bb9a86c05dd
Petr Abdulin <pabdulin@gmail.com>
2019-11-14 18:21:13 +0700

    Problem 7 solution complete.

--- Problem007.Lib/Problem.cs
+++ Problem007.Lib/Problem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem007.Lib
 {
@@ -12,12 +13,42 @@ For example, the message '111' would give 3, since it could be decoded as 'aaa',
 
 You can assume that the messages are decodable. For example, '001' is not allowed.
      */
-    
+
     public class Problem
     {
         public static int CountPossibleWaysToDecode(string message)
         {
-            return 0;
+            if (message == string.Empty)
+            {
+                return 0;
+            }
+            var result = CountRec(message);
+            return result;
+        }
+
+        private static int CountRec(string message, int count = 0)
+        {
+            if (message.Length == 0)
+            {
+                return count + 1;
+            }
+            else
+            {
+                var candidate1Reminder = message.Substring(1);
+                count = CountRec(candidate1Reminder, count);
+                if (message.Length > 1)
+                {
+                    var candidate2 = message.Substring(0, 2);
+                    var c2 = int.Parse(candidate2);
+                    if (c2 <= 26)
+                    {
+                        var candidate2Reminder = message.Substring(2);
+                        count = CountRec(candidate2Reminder, count);
+                    }
+                }
+            }
+
+            return count;
         }
     }
 }
--- Problem007.Tests/ProblemTest.cs
+++ Problem007.Tests/ProblemTest.cs
@@ -23,5 +23,47 @@ You can assume that the messages are decodable. For example, '001' is not allowe
             var result = Problem.CountPossibleWaysToDecode("111");
             Assert.AreEqual(3, result);
         }
+
+        [TestMethod]
+        public void AnotherTest1()
+        {
+            var result = Problem.CountPossibleWaysToDecode("1111");
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void AnotherTest2()
+        {
+            var result = Problem.CountPossibleWaysToDecode("1511");
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void AnotherTest3()
+        {
+            var result = Problem.CountPossibleWaysToDecode("262");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void SingleTest()
+        {
+            var result = Problem.CountPossibleWaysToDecode("2");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+            var result = Problem.CountPossibleWaysToDecode("");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void LongTest()
+        {
+            var result = Problem.CountPossibleWaysToDecode("1111111");
+            Assert.AreEqual(21, result);
+        }
     }
 }
