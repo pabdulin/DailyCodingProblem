7a1775562ad523362090945c3812387db230040f
Petr Abdulin <pabdulin@gmail.com>
2019-11-21 01:15:17 +0700

    Problem 10 complete.

---
+++ Problem010/Problem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Problem010
+{
+    /*
+     * Daily Coding Problem: Problem #10 [Medium]
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+     */
+    public class Problem
+    {
+        public static Timer ScheduleTask(int delayMs, Action func)
+        {
+            return new Timer(x => func.Invoke(), null, delayMs, Timeout.Infinite);
+        }
+    }
+}
---
+++ Problem010/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Problem010
+{
+    /*
+     * Daily Coding Problem: Problem #10 [Medium]
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before ScheduleTask..");
+            var timer = Problem.ScheduleTask(1000, () => { Console.WriteLine("From ScheduleTask!"); });
+            Console.WriteLine("After ScheduleTask..");
+            Thread.Sleep(2000);
+            timer.Dispose();
+        }
+    }
+}
